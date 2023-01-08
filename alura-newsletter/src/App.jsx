d2b1669e3bdbc567c376f15cdfe12e977edd61b2
Your Name3 <you@example.com>
2023-01-08 12:14:07 -0300

    inicio do projeto newsletter em react com tailwindCss

---
+++ alura-newsletter/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center text-green-500 mt-5'>Hello Tailwind</h1>
+    </div>
+  )
+}
+
+export default App
