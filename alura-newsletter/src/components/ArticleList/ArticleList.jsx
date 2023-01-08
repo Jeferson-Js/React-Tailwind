99862c7252c85dc91c4f11ba9436ecb9a932b4fb
Your Name3 <you@example.com>
2023-01-08 14:08:11 -0300

    Estilizando o layout dos cards da newsletter.

--- alura-newsletter/src/components/ArticleList/ArticleList.jsx
+++ alura-newsletter/src/components/ArticleList/ArticleList.jsx
@@ -1,10 +1,11 @@
 import data from '../../../articles.json'
+import Article from '../Article/Article'
 
 const ArticleList = () => {
   return (
-    <div className='mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-flow-cols-2 lg:grid-cols-3'>
+    <div className='mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-flow-cols-2 lg:grid-cols-3 lg:max-w-[1000px]'>
         {
-            data.map(props => <p>{props.title}</p>)
+            data.map((props, index) => <Article key={index} {...props} />)
         }
 
     </div>
@@ -12,3 +13,4 @@ const ArticleList = () => {
 }
 
 export default ArticleList
+
